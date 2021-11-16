9e27cea0d349d1d3072dfdbfba01240ff3ce40c3
mdufour <mathieu.dufour@whereyoulove.com>
2021-11-16 15:10:44 +0100

    Init db script

---
+++ backend/initDb.js
@@ -0,0 +1,41 @@
+/**
+ * Init DB mongo with mongoose
+ */
+
+const mongoose = require('mongoose');
+const User = require('./models/user');
+
+const bcrypt = require('bcrypt');
+const saltRounds = 10;
+
+// Generate two users - one admin and one basic
+async function generateUsers() {
+  await User.create({
+    pseudonym: 'Admin',
+    email: 'admin@email.com',
+    hashedPassword: await bcrypt.hash('adminpassword', saltRounds),
+    role: 'admin'
+  });
+  console.log('Admin user created successfully')
+
+  await User.create({
+    pseudonym: 'User',
+    email: 'user@email.com',
+    hashedPassword: await bcrypt.hash('userpassword', saltRounds),
+    role: 'admin'
+  });
+  console.log('Basic user created successfully');
+}
+
+mongoose
+.connect(`mongodb://localhost:27017/commentary`, {useNewUrlParser: true, useUnifiedTopology: true})
+.then(async () => {
+  console.log('MongoDB connected on mongodb://localhost:27017/commentary');
+  await generateUsers();
+  console.log('Done db initialisation');
+
+  // disconnect db
+  mongoose.connection.close();
+  console.log('MongoDB disconnected.');
+})
+.catch(err => console.log(err));
